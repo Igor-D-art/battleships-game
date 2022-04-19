8cd4dcd0b4a2c4107650a5a245b516a2a9a1e0a2
Igor-D-art <gorgsemenon1@gmail.com>
2022-04-19 21:05:20 +0300

    Added prettier and some webpack configurations

---
+++ dist/bundle.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+consolee.log('Hi there!!!')
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7O0FBQUEsMkIiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9iYXR0bGVzaGlwcy1nYW1lLy4vc3JjL2luZGV4LmpzIl0sInNvdXJjZXNDb250ZW50IjpbImNvbnNvbGVlLmxvZygnSGkgdGhlcmUhISEnKSJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
